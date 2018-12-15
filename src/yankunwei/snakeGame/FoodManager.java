6c71a0f1095ad3203b7023d587524cf61fbc7b7d
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2018-12-15 13:01:54 +0800

    添加食物管理器类

---
+++ src/yankunwei/snakeGame/FoodManager.java
@@ -0,0 +1,44 @@
+package yankunwei.snakeGame;
+
+import java.awt.*;
+import java.awt.geom.RectangularShape;
+import java.util.Random;
+import java.util.Vector;
+
+public class FoodManager {
+    private static final int DEFAULT_FOOD_WIDTH = 10;
+    private static final int DEFAULT_FOOD_HEIGHT = 10;
+    private static final int DEFAULT_PLUS_EDGE_WIDTH = 4;
+    private final Random random;
+    private Dimension frameBorder;
+    Vector<Food> foods;
+
+    public FoodManager(Dimension frameBorder) {
+        this.foods = new Vector<>();
+        this.frameBorder = frameBorder;
+        this.random = new Random(System.nanoTime());
+        for (int i = 0; i < 10; i++) {
+            Food food = new Food(random.nextInt(frameBorder.width), random.nextInt(frameBorder.height), DEFAULT_FOOD_WIDTH, DEFAULT_FOOD_HEIGHT, DEFAULT_PLUS_EDGE_WIDTH);
+            this.foods.add(food);
+        }
+    }
+
+    public Food canEatFood(RectangularShape snake) {
+        for (Food food: foods) {
+            if (food.intersects(snake.getFrame())) {
+                return food;
+            }
+        }
+        return null;
+    }
+
+    public void eatFood(Food food) {
+        foods.remove(food);
+    }
+
+    public void paintFood(Graphics2D graphics2D) {
+        for (Food food: foods) {
+            graphics2D.draw(food);
+        }
+    }
+}
