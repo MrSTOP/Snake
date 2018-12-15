9da94b8c13df33e9dd40d1994010c5f310fec30f
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2018-12-15 12:30:43 +0800

    清理试验代码

--- src/yankunwei/snakeGame/SnakeGame.java
+++ src/yankunwei/snakeGame/SnakeGame.java
@@ -5,19 +5,6 @@ import java.awt.*;
 
 public class SnakeGame {
     public static void main(String[] args) {
-//        Frame frame = new Frame();
-//        Button button = new Button("JJ");
-//        button.addActionListener(event -> {
-//            System.out.println("JJ");
-//        });
-//        frame.add(button);
-//        frame.setVisible(true);
-
-//
-//            JFrame snakeFrame = new SnakeFrame();
-//            snakeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//            snakeFrame.setVisible(true);
-
         EventQueue.invokeLater(() -> {
             JFrame snakeFrame = new SnakeFrame();
             snakeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
