86f45f4f711eee308264b548d5f28b40c4651e0b
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2018-12-15 21:35:34 +0800

    为游戏添加观感调整对话框

--- src/yankunwei/snakeGame/LookAndFeelDialog.java
+++ src/yankunwei/snakeGame/LookAndFeelDialog.java
@@ -1,7 +1,23 @@
 package yankunwei.snakeGame;
 
+import yankunwei.util.LookAndFeelHelper;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class LookAndFeelDialog extends JDialog {
-    
+    public LookAndFeelDialog(Frame owner) {
+        super(owner, true);
+        this.setTitle("观感");
+        JPanel panel = new JPanel(new GridLayout(3, 3, 10, 10));
+        JButton[] buttons = LookAndFeelHelper.getFeelAndLookButtonArray(owner, this);
+        for (JButton button : buttons) {
+            panel.add(button);
+        }
+        JButton ok = new JButton("OK");
+        ok.addActionListener(event -> this.setVisible(false));
+        panel.add(ok);
+        this.add(panel);
+        this.pack();
+    }
 }
