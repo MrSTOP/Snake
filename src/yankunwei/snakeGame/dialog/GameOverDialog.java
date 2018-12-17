12d10b2674164e27a724d36dc841445bad861f34
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2018-12-17 18:04:34 +0800

    添加游戏结束对话框(WIP)

---
+++ src/yankunwei/snakeGame/dialog/GameOverDialog.java
@@ -0,0 +1,12 @@
+package yankunwei.snakeGame.dialog;
+
+import javax.swing.*;
+
+/**
+ * Description: 游戏结束对话框
+ * @author 闫坤炜
+ * Date: 2018-12-17 18:02
+ */
+public class GameOverDialog extends JDialog {
+
+}
