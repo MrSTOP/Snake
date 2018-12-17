40616cfc708f6d115cff974ff59f2212670bafed
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2018-12-17 20:34:15 +0800

    添加Heart食物

--- src/yankunwei/snakeGame/food/Heart.java
+++ src/yankunwei/snakeGame/food/Heart.java
@@ -1,10 +1,47 @@
 package yankunwei.snakeGame.food;
 
+import java.awt.geom.AffineTransform;
+import java.awt.geom.PathIterator;
+import java.awt.geom.Point2D;
+
 /**
  * Description: 心形食物，可以增加生命
- *
  * @author 闫坤炜
  * Date: 2018-12-17 18:11
  */
-public class Heart {
+public class Heart extends Food {
+    private static final int DEFAULT_HEIGHT = 20;
+    private static final int DEFAULT_WIDTH = 20;
+
+    public Heart(Point2D point2D) {
+        this(point2D.getX(), point2D.getY());
+    }
+
+    public Heart(double x, double y) {
+        this(x, y, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+
+    public Heart(double x, double y, double width, double height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public boolean contains(double x, double y) {
+        return false;
+    }
+
+    @Override
+    public boolean intersects(double x, double y, double w, double h) {
+        return false;
+    }
+
+    @Override
+    public PathIterator getPathIterator(AffineTransform at) {
+        return new HeartIterator(this, at);
+    }
+
+    @Override
+    public PathIterator getPathIterator(AffineTransform at, double flatness) {
+        return new HeartIterator(this, at);
+    }
 }
---
+++ src/yankunwei/snakeGame/food/HeartIterator.java
@@ -0,0 +1,118 @@
+package yankunwei.snakeGame.food;
+
+import java.awt.geom.AffineTransform;
+import java.awt.geom.PathIterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Description: 心形食物的路径遍历器
+ * @author 闫坤炜
+ * Date: 2018-12-17 18:24
+ */
+public class HeartIterator implements PathIterator {
+    private double startX;
+    private double startY;
+    private double ctrl1X;
+    private double ctrl1Y;
+    private double ctrl2X;
+    private double ctrl2Y;
+    private double ctrl3X;
+    private double ctrl3Y;
+    private int index;
+    private AffineTransform affineTransform;
+
+    public HeartIterator(Heart heart, AffineTransform affineTransform) {
+        this.startX = heart.getCenterX();
+        this.startY = heart.getY() + heart.getHeight() / 3;
+        this.ctrl1X = heart.getMinX() - heart.getWidth() / 3;
+        this.ctrl1Y = heart.getMinY() - heart.getHeight() / 3;
+        this.ctrl2X = this.startX;
+        this.ctrl2Y = heart.getMaxY();
+        this.ctrl3X = heart.getMaxX() + heart.getWidth() / 3;
+        this.ctrl3Y = this.ctrl1Y;
+        this.affineTransform = affineTransform;
+    }
+
+    @Override
+    public int getWindingRule() {
+        return 0;
+    }
+
+    @Override
+    public boolean isDone() {
+        return this.index > 3;
+    }
+
+    @Override
+    public void next() {
+        index++;
+    }
+
+    @Override
+    public int currentSegment(float[] coords) {
+        if (isDone()) {
+            throw new NoSuchElementException("Food iterator out of bounds");
+        }
+        if (index == 3) {
+            return SEG_CLOSE;
+        }
+        if (index == 0) {
+            coords[0] = (float) this.startX;
+            coords[1] = (float) this.startY;
+        }
+        if (index == 1) {
+            coords[0] = (float) this.startX;
+            coords[1] = (float) this.startY;
+            coords[2] = (float) this.ctrl1X;
+            coords[3] = (float) this.ctrl1Y;
+            coords[4] = (float) this.ctrl2X;
+            coords[5] = (float) this.ctrl2Y;
+        }
+        if (index == 2) {
+            coords[0] = (float) this.ctrl2X;
+            coords[1] = (float) this.ctrl2Y;
+            coords[2] = (float) this.ctrl3X;
+            coords[3] = (float) this.ctrl3Y;
+            coords[4] = (float) this.startX;
+            coords[5] = (float) this.startY;
+        }
+        if (affineTransform != null) {
+            affineTransform.transform(coords, 0, coords, 0, 1);
+        }
+        return (index == 0 ? SEG_MOVETO : SEG_CUBICTO);
+    }
+
+    @Override
+    public int currentSegment(double[] coords) {
+        if (isDone()) {
+        throw new NoSuchElementException("Food iterator out of bounds");
+    }
+        if (index == 3) {
+            return SEG_CLOSE;
+        }
+        if (index == 0) {
+            coords[0] = this.startX;
+            coords[1] = this.startY;
+        }
+        if (index == 1) {
+            coords[0] = this.startX;
+            coords[1] = this.startY;
+            coords[2] = this.ctrl1X;
+            coords[3] = this.ctrl1Y;
+            coords[4] = this.ctrl2X;
+            coords[5] = this.ctrl2Y;
+        }
+        if (index == 2) {
+            coords[0] = this.ctrl2X;
+            coords[1] = this.ctrl2Y;
+            coords[2] = this.ctrl3X;
+            coords[3] = this.ctrl3Y;
+            coords[4] = this.startX;
+            coords[5] = this.startY;
+        }
+        if (affineTransform != null) {
+            affineTransform.transform(coords, 0, coords, 0, 1);
+        }
+        return (index == 0 ? SEG_MOVETO : SEG_CUBICTO);
+    }
+}
