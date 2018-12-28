adb2003ffe7691d2ea78177a93827b53d3f97f6c
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2018-12-28 16:08:11 +0800

    添加窗口自动居中的功能

--- src/yankunwei/snakeGame/SnakeFrame.java
+++ src/yankunwei/snakeGame/SnakeFrame.java
@@ -3,6 +3,7 @@ package yankunwei.snakeGame;
 import yankunwei.util.SoundPlayer;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * 贪吃蛇窗口框架
@@ -10,9 +11,12 @@ import javax.swing.*;
 public class SnakeFrame extends JFrame {
     SnakePanel snakePanel;
     public SnakeFrame() {
-       SoundPlayer.getInstant().playSound("BGM");
-       this.snakePanel = new SnakePanel(this);
-       this.add(snakePanel);
-       this.pack();
+        SoundPlayer.getInstant().playSound("BGM");
+        this.snakePanel = new SnakePanel(this);
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension panelSize = snakePanel.getPreferredSize();
+        this.setLocation((screen.width - panelSize.width) / 2, (screen.height - panelSize.height) / 2);
+        this.add(snakePanel);
+        this.pack();
     }
 }
