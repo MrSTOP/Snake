2efac422a2173adaaa1acbbeba3c7c875a1d08ce
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2018-12-28 16:09:22 +0800

    添加关于对话框的大小

--- src/yankunwei/snakeGame/dialog/AboutDialog.java
+++ src/yankunwei/snakeGame/dialog/AboutDialog.java
@@ -4,10 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class AboutDialog extends JDialog {
+    private static final int ABOUT_DIALOG_WIDTH = 100;
+    private static final int ABOUT_DIALOG_HEIGHT = 100;
     public AboutDialog(Frame owner) {
         super(owner);
         JLabel label = new JLabel("作者：闫坤炜");
+        this.setLocationRelativeTo(owner);
         this.add(label);
-        this.pack();
+        this.setSize(ABOUT_DIALOG_WIDTH, ABOUT_DIALOG_HEIGHT);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(ABOUT_DIALOG_WIDTH, ABOUT_DIALOG_HEIGHT);
     }
 }
