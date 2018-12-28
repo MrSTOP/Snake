686d48d4027c0707617966582edd63c4b9f8ba7f
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2018-12-28 15:55:46 +0800

    为主界面添加网格袋布局管理器

--- src/yankunwei/snakeGame/MainInterface.java
+++ src/yankunwei/snakeGame/MainInterface.java
@@ -4,8 +4,10 @@ import yankunwei.snakeGame.dialog.AboutDialog;
 import yankunwei.snakeGame.dialog.LookAndFeelDialog;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainInterface extends JPanel {
+    private static final int BUTTON_GRID_X = 1;
     private SnakePanel parent;
     private JButton startGameButton;
     private JButton continueGameButton;
@@ -14,14 +16,21 @@ public class MainInterface extends JPanel {
     private JButton aboutGameButton;
     private JButton lookAndFeelButton;
 
+    private AboutDialog aboutDialog;
+    private LookAndFeelDialog lookAndFeelDialog;
+
     public MainInterface(SnakePanel parent) {
+        this.setLayout(new GridBagLayout());
         this.parent = parent;
         this.startGameButton = new JButton("开始游戏");
         this.continueGameButton = new JButton("继续游戏");
         this.exitGameButton = new JButton("退出游戏");
         this.scoreGameButton = new JButton("分数查看");
-        this.aboutGameButton = new JButton("关于");
+        this.aboutGameButton = new JButton("关    于");
         this.lookAndFeelButton = new JButton("观感设置");
+
+        this.aboutDialog = new AboutDialog(parent.getParentFrame());
+        this.lookAndFeelDialog = new LookAndFeelDialog(parent.getParentFrame());
 
         this.startGameButton.addActionListener(event -> {
             this.parent.startGame();
@@ -29,18 +38,62 @@ public class MainInterface extends JPanel {
         this.continueGameButton.addActionListener(event -> {
             this.parent.continueGame();
         });
+        this.exitGameButton.addActionListener(event -> {
+            System.exit(0);
+        });
         this.lookAndFeelButton.addActionListener(event -> {
-            new LookAndFeelDialog(parent.getParentFrame()).setVisible(true);
+            this.lookAndFeelDialog.setVisible(true);
         });
         this.aboutGameButton.addActionListener(event -> {
-            new AboutDialog(parent.getParentFrame()).setVisible(true);
+            this.aboutDialog.setVisible(true);
         });
+        init();
+    }
 
-        this.add(startGameButton);
-        this.add(continueGameButton);
-        this.add(exitGameButton);
-        this.add(scoreGameButton);
-        this.add(aboutGameButton);
-        this.add(lookAndFeelButton);
+    private void init() {
+        Insets insets = new Insets(10, 30, 10, 30);
+        GridBagConstraints startGBC = new GridBagConstraints();
+        startGBC.gridx = BUTTON_GRID_X;
+        startGBC.gridy = 0;
+        startGBC.insets = insets;
+        startGBC.ipadx = 50;
+        startGBC.ipady = 50;
+        GridBagConstraints continueGBC = new GridBagConstraints();
+        continueGBC.gridx = BUTTON_GRID_X;
+        continueGBC.gridy = 2;
+        continueGBC.insets = insets;
+        continueGBC.ipadx = 50;
+        continueGBC.ipady = 50;
+        GridBagConstraints exitGBC = new GridBagConstraints();
+        exitGBC.gridx = BUTTON_GRID_X;
+        exitGBC.gridy = 3;
+        exitGBC.insets = insets;
+        exitGBC.ipadx = 50;
+        exitGBC.ipady = 50;
+        GridBagConstraints scoreGBC = new GridBagConstraints();
+        scoreGBC.gridx = BUTTON_GRID_X;
+        scoreGBC.gridy = 4;
+        scoreGBC.insets = insets;
+        scoreGBC.ipadx = 50;
+        scoreGBC.ipady = 50;
+        GridBagConstraints aboutGBC = new GridBagConstraints();
+        aboutGBC.gridx = BUTTON_GRID_X;
+        aboutGBC.gridy = 5;
+        aboutGBC.insets = insets;
+        aboutGBC.ipadx = 50;
+        aboutGBC.ipady = 50;
+        GridBagConstraints lookAndFeelGBC = new GridBagConstraints();
+        lookAndFeelGBC.gridx = BUTTON_GRID_X;
+        lookAndFeelGBC.gridy = 6;
+        lookAndFeelGBC.insets = insets;
+        lookAndFeelGBC.ipadx = 50;
+        lookAndFeelGBC.ipady = 50;
+        this.add(startGameButton, startGBC);
+        this.add(continueGameButton, continueGBC);
+        this.add(exitGameButton, exitGBC);
+        this.add(scoreGameButton, scoreGBC);
+        this.add(aboutGameButton, aboutGBC);
+        this.add(lookAndFeelButton, lookAndFeelGBC);
+
     }
 }
