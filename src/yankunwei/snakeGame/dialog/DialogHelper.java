931a4f7096433fcc93d656e8ce0131401106731e
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2018-12-28 15:52:05 +0800

    添加游戏结束对话框

---
+++ src/yankunwei/snakeGame/dialog/DialogHelper.java
@@ -0,0 +1,10 @@
+package yankunwei.snakeGame.dialog;
+
+/**
+ * Description: 对话框工具类
+ *
+ * @author 闫坤炜
+ * Date: 2018-12-21 17:16
+ */
+public class DialogHelper {
+}
--- src/yankunwei/snakeGame/dialog/GameOverDialog.java
+++ src/yankunwei/snakeGame/dialog/GameOverDialog.java
@@ -1,6 +1,7 @@
 package yankunwei.snakeGame.dialog;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Description: 游戏结束对话框
@@ -8,5 +9,27 @@ import javax.swing.*;
  * Date: 2018-12-17 18:02
  */
 public class GameOverDialog extends JDialog {
+    Font font;
+    JLabel label;
+    int score;
+    public GameOverDialog(Frame owner, int score) {
+        super(owner);
+        this.setLayout(new GridLayout(2, 1));
+        this.font = new Font("Serif", Font.PLAIN, 60);
+        this.label = new JLabel("总分：" + String.valueOf(score) + "分");
+        this.label.setFont(font);
+        JLabel gameOver = new JLabel("Game Over!");
+        gameOver.setFont(font);
+        gameOver.setForeground(Color.RED);
+        this.add(gameOver);
+        this.add(label);
+        this.setLocationRelativeTo(owner);
+        this.pack();
+    }
 
+    public void setScore(int score) {
+        this.score = score;
+        this.label.setText("总分：" + this.score + "分");
+        this.pack();
+    }
 }
