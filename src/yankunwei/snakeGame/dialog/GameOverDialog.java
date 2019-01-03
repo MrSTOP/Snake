384b50b93a46b5b4c142c8d9bc390058e68e50bb
MrSTOP <37563272+MrSTOP@users.noreply.github.com>
2019-01-03 20:55:23 +0800

    将原JTextField替换为有PlaceHolder功能的NameTextField，并添加将分数添加到数据库的功能

--- src/yankunwei/snakeGame/dialog/GameOverDialog.java
+++ src/yankunwei/snakeGame/dialog/GameOverDialog.java
@@ -1,5 +1,7 @@
 package yankunwei.snakeGame.dialog;
 
+import yankunwei.snakeGame.SQLiteDatabase;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,28 +10,52 @@ import java.awt.*;
  * @author 闫坤炜
  * Date: 2018-12-17 18:02
  */
-public class GameOverDialog extends JDialog {
+public class GameOverDialog extends CommonDialog {
     Font font;
     JLabel label;
+    JTextField textField;
+    Frame owner;
+    JButton confirm;
     int score;
     public GameOverDialog(Frame owner, int score) {
         super(owner);
-        this.setLayout(new GridLayout(2, 1));
+        this.owner = owner;
+        this.setLayout(new GridLayout(4, 1));
         this.font = new Font("Serif", Font.PLAIN, 60);
-        this.label = new JLabel("总分：" + String.valueOf(score) + "分");
+        this.label = new JLabel("总分：" + score + "分");
+        this.confirm = new JButton("确认");
+        this.confirm.addActionListener(event -> {
+            String playerName = textField.getText().trim();
+            if (playerName.isEmpty()) {
+                JOptionPane.showMessageDialog(this, "玩家名不能为空！", "警告", JOptionPane.WARNING_MESSAGE);
+            }
+            else{
+                SQLiteDatabase.insertScore(playerName, this.score);
+                this.setVisible(false);
+            }
+        });
+        this.label.setHorizontalAlignment(JLabel.CENTER);
+        this.textField = new NameTextField(15, "请输入玩家昵称");
         this.label.setFont(font);
+        this.textField.setFont(font);
+
         JLabel gameOver = new JLabel("Game Over!");
         gameOver.setFont(font);
         gameOver.setForeground(Color.RED);
+        gameOver.setHorizontalAlignment(JLabel.CENTER);
         this.add(gameOver);
         this.add(label);
-        this.setLocationRelativeTo(owner);
+        this.add(textField);
+        this.add(confirm);
+        this.relocation();
         this.pack();
     }
 
     public void setScore(int score) {
         this.score = score;
         this.label.setText("总分：" + this.score + "分");
+        this.textField.setText("");
         this.pack();
+        this.relocation();
     }
 }
